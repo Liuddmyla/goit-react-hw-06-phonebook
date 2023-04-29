1555d571a1ddf2c1d8887a1cb9151dbc097dd6e2
Liuddmyla <112398546+Liuddmyla@users.noreply.github.com>
2023-04-29 21:09:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={store.persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
